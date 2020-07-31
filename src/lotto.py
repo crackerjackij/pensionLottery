90aec5e9a45a4b386b48c81aacc7f27031aba608
crackerjackij <saltkeeper@hanmail.net>
2020-07-31 16:01:35 +0900

    로또 1~45까지 랜덤한 숫자만 뽑이도록 변경

--- src/lotto.py
+++ src/lotto.py
@@ -1,15 +1,17 @@
 import random
 
-numbers = list(range(1,46))
-print("원본 리스트: \n", numbers, "\n")
+# 랜덤한 로또 숫자 뽑기
+# 번호는 1에서 45까지 존재
 
-weights_list = [1]*9 + [2]*9 + [3]*9 + [4]*9 + [5]*9
-print("가중치 배열: \n", weights_list, "\n")
+numbers = list(range(1,46))
+#print("원본 리스트: \n", numbers, "\n")
 
 # 가중치 미적용
 numbers_even = random.sample(numbers, 6)
-print("추출 리스트(가중치 없음): ", sorted(numbers_even))
+print("추출 리스트: ", sorted(numbers_even))
 
 # 가중치 적용
-numbers_weighted = random.choices(numbers, weights=weights_list, k=6)
-print("추출 리스트(가중치 적용): ", sorted(numbers_weighted))
+#weights_list = [1]*9 + [2]*9 + [3]*9 + [4]*9 + [5]*9
+#print("가중치 배열: \n", weights_list, "\n")
+#numbers_weighted = random.choices(numbers, weights=weights_list, k=6)
+#print("추출 리스트(가중치 적용): ", sorted(numbers_weighted))
